23c570865d60eeb7c8cb756b9fb486c2a8747ff9
jsdeherrera <jordan.deherrera@gmail.com>
2015-06-07 17:58:59 -0500

    Updated with outline

--- Weightlifting.R
+++ Weightlifting.R
@@ -3,3 +3,14 @@ library(rmarkdown)
 library(caret)
 library(ggplot2)
 
+#import data set
+df <- read.csv("pml-training.csv")
+
+#clean data frame
+cleandf <- na.omit(df)
+
+#Create training and testing sets
+inTrain <- createDataPartition(y=cleandf$classe, p=0.7, list=FALSE)
+Training <- cleandf[inTrain,]
+Testing <- cleandf[-inTrain,]
+
